d5c49aa73db9c981a1f21afb40984b933d058db3
j-prt <peart.l.joe@gmail.com>
2024-10-31 16:42:44 -0700

    write mult in hack assembly

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+// pseudocode
+
+// R2 = R0 * R1
+// acc = 0
+// for i in range(R1):
+//   acc = acc + R0
+// R2 = acc
+
+// Set up required variables
+@R2
+M=0
+@R1
+D=M
+@i
+M=0
+
+// Main loop
+(LOOP)
+// End condition
+@i
+D=M
+@R1
+D=M-D
+@END
+D;JEQ
+
+// Else, accumulate
+@R0
+D=M
+@R2
+M=M+D
+
+// Increment
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+// End
+(END)
+@END
+0;JMP
