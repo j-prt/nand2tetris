55da70eb6ce3df3bf5134153aabaf4ad3e96234c
j-prt <peart.l.joe@gmail.com>
2024-11-16 09:29:03 -0800

    checkpoint

--- software/assembler.py
+++ software/assembler.py
@@ -2,18 +2,25 @@
 
 import sys
 
+
 # Get the path to the .asm
-try:
-    file_path = sys.argv[1]
-except IndexError:
-    print('Error: no filepath provided.')
-    # exit(1)
+def get_path():
+    try:
+        file_path = sys.argv[1]
+    except IndexError:
+        print('Error: no filepath provided.')
+        exit(1)
+    else:
+        return file_path
 
 
-# For parsing A-instructions
-def int_to_bin(num):
+# For parsing A-instruction addresses
+def int_to_bin(addr):
     """Converts an integer to its binary format, padded to 15 digits."""
-    return f'{num:b}'.rjust(15, '0')
+    return f'{addr:b}'.rjust(15, '0')
 
 
-print(int_to_bin(5))
+def build_a(addr):
+    """Completes A-instructions when given an int address"""
+    addr = int_to_bin(addr)
+    return '0' + addr
